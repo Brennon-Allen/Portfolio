9b52390ed20a6d3529523ad6c4e40fb898ef368e
Brennon Allen <brennon.t.allen@gmail.com>
2024-09-15 22:03:26 -0500

    init

--- src/components/WebApps.jsx
+++ src/components/WebApps.jsx
@@ -3,9 +3,9 @@ export default function WebApps(){
         <div className="webapps">
             <h1>Overview</h1>
             <div className="webappsList">
-                <div className="animated--box"><h1 className="stat--heading">5</h1></div>
+                <div className="typewriter"><h1>5</h1></div>
                 <p className="projects--text">PROJECTS COMPLETED</p>
-                <div className="animated--box"><h1 className="stat--heading">2</h1></div>
+                <div className="typewriter"><h1>2</h1></div>                  
                 <p className="projects--text">ONGOING PROJECTS</p>
             </div>
         </div>
