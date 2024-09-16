ba1ddfd668179114bf00ae4a1615db8fc8aabd6b
Brennon Allen <brennon.t.allen@gmail.com>
2024-09-16 11:48:35 -0500

    init

--- src/components/HeaderInfo.jsx
+++ src/components/HeaderInfo.jsx
@@ -2,7 +2,7 @@ export default function HeaderInfo(){
     return (
         <div className="navbar">
             <h2 className="header--name">Brennon Allen</h2>
-            <p className="header--email">GeneralBusinessData@gmail.com<br></br>+1 726-400-3411</p>
+            <p className="header--email">Brennon.T.Allen@Gmail.com<br></br>+1 726-400-3411</p>
         </div>
     )
 }
